8a7d45f66b89f93c0dc176cda054a4d1cf5879c2
venkatasujatha <valluru.1sujatha@gmail.com>
2023-01-09 16:50:20 +0530

    modified

--- controller/controller.js
+++ controller/controller.js
@@ -4,17 +4,18 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const add = async(req,res)=>{
     try {
-       // const {userName,password} =req.body;
-        const user =await prisma.controller.createMany ({
+       
+        const user =await prisma.user.create ({
             data:req.body
         });
-        res.json(Created);
+        //res.json(user);
         res.status(200).json({
             message:"record inserted successfully",
             Response:user
         })
         
     } catch (error) {
+        console.log(error)
         res.status(400).json({
             message:"record not inserted successfully",
             Response:error.message
--- prisma/migrations/20230109111218_init/migration.sql
+++ prisma/migrations/20230109111218_init/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
 CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `userName` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `token` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_userName_key`(`userName`),
     PRIMARY KEY (`id`)
